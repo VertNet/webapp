47a37b801abc15dfac41af401f7f2640d6ad435d
Aaron Steele <eightysteele@gmail.com>
2013-08-13 20:05:47 -0700

    update about

--- client/vertnet/views/about.js
+++ client/vertnet/views/about.js
@@ -12,6 +12,12 @@
   ], function ($, _, Backbone, mps, rpc, template) {
 
     return Backbone.View.extend({
+      events: {
+        'click #search-link': 'onSearchClick',
+        'click #advanced-link': 'onAdvancedSearchClick',
+        'click #publishers-link': 'onPublishersClick'
+      },
+
       initialize: function (options, app) {
         this.app = app;
         mps.publish('spin', [true]);
@@ -24,6 +30,22 @@
 
       setup: function() {
         mps.publish('spin', [false]);
-      }
+      },
+
+      onSearchClick: function(e) {
+        e.preventDefault();
+        mps.publish('navigate', [{path: 'search', trigger: true}]);        
+      },
+
+      onAdvancedSearchClick: function(e) {
+        e.preventDefault();
+        mps.publish('navigate', [{path: 'search?advanced=1', trigger: true, replace: false}]);        
+      },
+
+      onPublishersClick: function(e) {
+        e.preventDefault();
+        mps.publish('navigate', [{path: 'publishers', trigger: true}]);        
+      },
+
   });
 });
