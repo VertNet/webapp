97a570358a33ea1dda570e9f1d447822e8b72eb1
laurarussell <laura.anne.russell@gmail.com>
2014-09-19 16:30:25 -0500

    changed format for pubdate

--- www/js/app/models/resource.js
+++ www/js/app/models/resource.js
@@ -44,7 +44,9 @@ define([
 
     getDate: function() {
       var splits = this.get('pubdate').split(' ');
-      return splits[1] + ' ' + splits[2] + ', ' + splits[5];
+     // return splits[1] + ' ' + splits[2] + ', ' + splits[5];
+	 return splits[5] + '-' + splits[1] + '-' + splits[2];
+	 return splits;
     }
   });
 });
