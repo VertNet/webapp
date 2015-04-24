591feb7be468c2d6b76f7de277bedb6fd75eeb98
Javier Otegui <javier.otegui@gmail.com>
2015-04-24 23:33:09 +0200

    Fix #544

--- vertnet/service/stats.py
+++ vertnet/service/stats.py
@@ -8,13 +8,24 @@ import logging
 
 
 def main(environ, start_response):
-
+    
+    status = 200
+    headers={}
+    start_response(status, headers)
+    
     path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'cdbkey.txt')
     api_key = open(path, "r").read().rstrip()
-    logging.info("CARTODB KEY %s" % key)
+    logging.info("CARTODB KEY %s" % api_key)
 
-    url = "https://vertnet.cartodb.com/api/v2/sql?api_key={0}&q=select * from daily_portal_stats where created_at=max(created_at)".format(api_key)
-    d = json.loads(urlopen(urlencode(url)).read())['rows']
-    console.log(d)
-    
-    return str(d)
+    url = "https://vertnet.cartodb.com/api/v2/sql"
+    q = 'select * from daily_portal_stats order by created_at desc limit 1'
+
+    params = {'api_key':api_key, 'q':q}
+    data = urlencode(params)
+
+    raw = urlopen(url, data=data).read()
+    d = json.loads(raw)['rows'][0]
+    logging.info(d.keys())
+
+#    return [str(d)]
+    return [str(d['mindate']), "|", str(d['created_at']), "|", str(d['searches']), "|", str(d['records_viewed']), "|", str(d['downloads']), "|", str(d['records_downloaded']), "|", str(d['institution_data'])[1:-1], "|", str(d['class_data'])[1:-1], "|", str(d['download_data'])[1:-1]]
