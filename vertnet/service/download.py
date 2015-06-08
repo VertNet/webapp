7a33a13c1761138eeeeab4b5561b1bdd5fd47da0
tucotuco <tuco@berkeley.edu>
2015-06-08 18:50:40 -0300

    Correcting Google Cloud Storage URL to avoid requiring login.

--- vertnet/service/download.py
+++ vertnet/service/download.py
@@ -86,7 +86,7 @@ class WriteHandler(webapp2.RequestHandler):
             mail.send_mail(sender="VertNet Downloads <eightysteele@gmail.com>", 
                 to=email, subject="Your VertNet download is ready!",
                 body="""
-You can download "%s" here within the next 24 hours: https://storage.cloud.google.com/vn-downloads2/%s
+You can download "%s" here within the next 24 hours: https://storage.googleapis.com/vn-downloads2/%s
 """ % (name, filename.split('/')[-1]))
 
 class DownloadHandler(webapp2.RequestHandler):
