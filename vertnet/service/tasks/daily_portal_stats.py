70972b5ce9debc120a067e3859d0035c7e950f69
Javier Otegui <javier.otegui@gmail.com>
2015-08-04 18:05:39 +0200

    Failed attempt to fix memory leak

--- vertnet/service/tasks/daily_portal_stats.py
+++ vertnet/service/tasks/daily_portal_stats.py
@@ -20,11 +20,23 @@ def apikey():
     return key
 api_key=apikey()
 
+# CartoDB requests
+def cartodb(query):
+    """Launch a query to CartoDB."""
+    urlbase = "https://vertnet.cartodb.com/api/v2/sql"
+    params = {
+        'api_key': api_key,
+        'q': query
+    }
+    url = '?'.join([urlbase, urllib.urlencode(params)])
+    logging.info('QUERY URL: {}'.format(url))
+    d = json.loads(urllib2.urlopen(url).read())['rows']
+    return d
+
 # Get actual stats from query_log_master
 def getExplicitStuff(tag):
-    url = "https://vertnet.cartodb.com/api/v2/sql?api_key={2}&q=select+query,%20sum%28count%29+from+query_log_master+where+query+like+%27%25{0}%3A%25%27%20and%20created_at%3E=date%20%27{1}%27%20group%20by%20query".format(tag, query_date_limit, api_key)
-    logging.info('QUERY URL: %s' % url)
-    d = json.loads(urllib2.urlopen(url).read())['rows']
+    query = "select query, sum(count) from query_log_master where query like '%{0}:%' and created_at>=date '{1}' group by query".format(tag, query_date_limit)
+    d = cartodb(query)
     stuffQ = {}
     stuffR = {}
     stuffSgood = []
@@ -53,11 +65,10 @@ def getExplicitStuff(tag):
 
 def getDownloadsData():
     # Monthly
-    url = "https://vertnet.cartodb.com/api/v2/sql?api_key={1}&q=select%20concat%28year,%27-%27,month%29%20as%20date,%20queries,%20records%20from%20%28%20select%20extract%28month%20from%20date%28created_at%29%29%20as%20month,%20extract%28year%20from%20date%28created_at%29%29%20as%20year,%20count%28*%29%20as%20queries,%20sum%28count%29%20as%20records%20from%20query_log_master%20where%20type=%27download%27%20and%20created_at%3E=date%20%27{0}%27%20group%20by%20extract%28month%20from%20date%28created_at%29%29,%20extract%28year%20from%20date%28created_at%29%29%20order%20by%20extract%28year%20from%20date%28created_at%29%29,%20extract%28month%20from%20date%28created_at%29%29%29%20as%20foo".format(query_date_limit, api_key)
+    query = "select concat(year,'-',month) as date, queries, records from ( select extract(month from date(created_at)) as month, extract(year from date(created_at)) as year, count(*) as queries, sum(count) as records from query_log_master where type='download' and created_at>=date '{0}' group by extract(month from date(created_at)), extract(year from date(created_at)) order by extract(year from date(created_at)), extract(month from date(created_at))) as foo".format(query_date_limit)
     # Daily
     #url = "https://vertnet.cartodb.com/api/v2/sql?api_key={1}q=select%20date%28created_at%29,%20count%28*%29%20as%20queries,%20sum%28count%29%20as%20records%20from%20query_log_master%20where%20type=%27download%27%20and%20created_at%3E=date%20%27{0}%27%20group%20by%20date%28created_at%29%20order%20by%20date%28created_at%29".format(query_date_limit, api_key)
-    logging.info('QUERY URL: %s' % url)
-    d = json.loads(urllib2.urlopen(url).read())['rows']
+    d = cartodb(query)
     downloadsdata = []
     for i in d:
         date = str(i['date'])
@@ -66,9 +77,8 @@ def getDownloadsData():
     return downloadsdata
 
 def getMetadata():
-    url = "https://vertnet.cartodb.com/api/v2/sql?api_key={1}&q=select%20type,%20count%28*%29%20as%20searches,%20sum%28count%29%20as%20records%20from%20query_log_master%20where%20created_at%3E=date%20%27{0}%27%20group%20by%20type".format(query_date_limit, api_key)
-    logging.info('QUERY URL: %s' % url)
-    d = json.loads(urllib2.urlopen(url).read())['rows']
+    query = "select type, count(*) as searches, sum(count) as records from query_log_master where created_at>=date '{0}' group by type".format(query_date_limit)
+    d = cartodb(query)
     metadata = {}
     for i in d:
         t = i['type']
@@ -78,23 +88,33 @@ def getMetadata():
     return metadata
 
 def getRecordsQueried():
-    url = "https://vertnet.cartodb.com/api/v2/sql?api_key={1}&q=select%20results_by_resource%20from%20query_log_master%20where%20client=%27portal-prod%27%20and%20results_by_resource%20is%20not%20null%20and%20created_at%3E=date%20%27{0}%27%20and%20results_by_resource%20%3C%3E%20%27%7B%7D%27".format(query_date_limit, api_key)
-    logging.info('QUERY URL: %s' % url)
-    d = json.loads(urllib2.urlopen(url).read())['rows']
     records_queried = 0
-    for x in list(range(len(d))):
-        try:
-            records_queried += sum(json.loads(d[x]['results_by_resource']).values())
-        except:
-            pass
-    #records_queried = sum([sum(json.loads(d[x]['results_by_resource']).values()) for x in list(range(len(d)))])
+    query = "select count(*) from query_log_master where client='portal-prod' and results_by_resource is not null and created_at>=date '%s' and results_by_resource <> '{}'" % (query_date_limit)
+    count = cartodb(query)[0]['count']
+    
+    for r in [1, 2, 3]:
+        if r == 1:
+            limit_string = "limit {0}".format(count/3)
+        elif r == 2:
+            limit_string = "limit {0} offset {0}".format(count/3)
+        elif r == 3:
+            limit_string = "offset {0}".format(count/3*2)
+
+        logging.info("ROUND {0}".format(r))
+        query = "select results_by_resource from query_log_master where client='portal-prod' and results_by_resource is not null and created_at>=date '%s' and results_by_resource <> '{}' %s" % (query_date_limit, limit_string)
+        d = cartodb(query)
+        for x in list(range(len(d))):
+            try:
+                records_queried += sum(json.loads(d[x]['results_by_resource']).values())
+            except:
+                pass
+
     logging.info("{0} records queried".format(records_queried))
     return records_queried
 
 def getMaxDate():
-    url = "https://vertnet.cartodb.com/api/v2/sql?api_key={0}&q=select%20max%28created_at%29%20from%20query_log_master".format(api_key)
-    logging.info('QUERY URL: %s' % url)
-    d = json.loads(urllib2.urlopen(url).read())['rows'][0]['max']
+    query = "select max(created_at) as d from query_log_master"
+    d = cartodb(query)[0]['d']
     
     formatin = '%Y-%m-%dT%H:%M:%SZ'
     formatout = '%b %d, %Y'
